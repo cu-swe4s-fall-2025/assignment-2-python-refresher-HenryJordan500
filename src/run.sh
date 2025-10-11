fe5e8e82ee36cf0ae7de3d2374d0b2759d570fdc
HenryJordan500 <henry.jordan@colorado.edu>
2025-10-10 20:51:00 -0600

    Changed run python commands as modules

--- src/run.sh
+++ src/run.sh
@@ -5,4 +5,4 @@ echo "Country column: $2"
 echo "Fires column: $3"
 echo "Filename: $4"
 
-python src/print_fires.py --country "$1" --country_column "$2" --fires_column "$3" --file_name "$4"
+python -m src.print_fires.py --country "$1" --country_column "$2" --fires_column "$3" --file_name "$4"
