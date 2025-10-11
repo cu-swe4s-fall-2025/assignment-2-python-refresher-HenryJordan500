50de73c7a40df5d557983814d8857a2ad3b852e6
HenryJordan500 <henry.jordan@colorado.edu>
2025-10-10 23:14:45 -0600

    cd in func test

--- test/func/test_print_fires.sh
+++ test/func/test_print_fires.sh
@@ -1,6 +1,8 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
+cd src
+
 run general_test python -m src.print_fires --country "Albania" --country_column 0 --fires_column 3 --file_name "func_test_data.csv"
 assert_in_stdout [7, 7, 7, 7, 7, 7, 13, 33, 6, 13, 77, 1, 6, 1, 2, 4, 0, 0, 2]
 assert_exit_code 0
